5988a9b9ad6a81676ead15f640b8cf1879267126
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-09-06 20:12:03 -0700

    Initial commit

---
+++ proj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Apollo_Archives_DB_Downloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
