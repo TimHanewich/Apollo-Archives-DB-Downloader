9d053b0edde29636a6b2bf8ee7896fa95c020697
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-09-06 20:41:22 -0700

    Added Apollo Log Entry

---
+++ proj/ApolloLogEntry.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ApolloArchives
+{
+    public class ApolloLogEntry
+    {
+        public string Title {get; set;}
+        public string Description {get; set;}
+        public string[] AssociatedImageIDs {get; set;}
+    }
+}
